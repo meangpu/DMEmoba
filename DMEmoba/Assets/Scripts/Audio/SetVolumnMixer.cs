26b309db08a8cfd82d82b17c7d147079c589ef1f
meangpu <meangpu@gmail.com>
2021-04-04 20:35:15 +0700

    update sound

--- DMEmoba/Assets/Scripts/Audio/SetVolumnMixer.cs
+++ DMEmoba/Assets/Scripts/Audio/SetVolumnMixer.cs
@@ -20,6 +20,5 @@ public class SetVolumnMixer : MonoBehaviour
     public void SetSoundEffectLevel(float sliderValue)
     {
         mixer.SetFloat("soundEffect", Mathf.Log10(sliderValue) * 20);
-        // FindObjectOfType<AudioManager>().Play("startButton2");
     }
 }
