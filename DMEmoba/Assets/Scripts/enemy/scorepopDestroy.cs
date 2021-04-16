d559fdba3da46d42656f228b31bbb6d3120ebb9a
meangpu <51825872+meangpu@users.noreply.github.com>
2021-04-16 16:31:00 +0700

    a

---
+++ DMEmoba/Assets/Scripts/enemy/scorepopDestroy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scorepopDestroy : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+}
